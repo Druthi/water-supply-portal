db984438dae867bb0556b341e78d9141741ac543
Druthi <polisettydruthi.k@gmail.com>
2019-07-11 16:59:02 -0500

    Express setup complete Please enter the commit message for your changes. Lines starting

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+let app = express();
+const morgan = require('morgan');
+var bodyParser = require('body-parser')
+
+
+app.use(morgan());
+app.use(bodyParser());
+app.use(express.static(__dirname + '/../client/dist'));
+
+
+let port = process.env.PORT || 3000;
+
+app.listen(port, function() {
+  console.log(`listening on port ${port}`);
+});
